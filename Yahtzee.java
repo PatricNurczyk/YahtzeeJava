a9555f1398d88a0a4e86339a190455eabb5d043c
PatricNurczyk <115892428+PatricNurczyk@users.noreply.github.com>
2023-11-18 12:57:09 -0500

    Added Starter Files
    Added Graphics and Yahtzee.java

---
+++ Yahtzee.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class Yahtzee {
+    public static void main(String [] args){
+        JFrame frame = new JFrame( "Yahtzee" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 500, 500 ); 
+        frame.setVisible( true );
+    }
+}
